6813cd94445728a8085f5b9510a30e274b799e8c
IsaacNZ <95659487+IsaacNZ@users.noreply.github.com>
2021-12-17 21:25:57 +1300

    Added chart to view spending by month

--- src/App.js
+++ src/App.js
@@ -13,21 +13,39 @@ const DUMMY_EXPENSES = [
   },
   {
     id: 'e2',
-    title: 'New TV',
+    title: 'TV',
     amount: 799.49,
     date: new Date(2021, 2, 12),
   },
   {
     id: 'e3',
-    title: 'Car Insurance',
+    title: 'Insurance',
     amount: 294.67,
     date: new Date(2021, 2, 28),
   },
   {
     id: 'e4',
-    title: 'New Desk (Wooden)',
+    title: 'Desk',
     amount: 450,
     date: new Date(2021, 5, 12),
+  },
+  {
+    id: 'e5',
+    title: 'Couch',
+    amount: 1250,
+    date: new Date(2021, 2, 14),
+  },
+  {
+    id: 'e6',
+    title: 'Laptop',
+    amount: 1450,
+    date: new Date(2021, 11, 2),
+  },
+  {
+    id: 'e7',
+    title: 'Table',
+    amount: 680,
+    date: new Date(2021, 8, 25),
   },
 ];
 
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,10 +4,10 @@ import './Expenses.css';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import ExpensesList from './ExpensesList';
-import { findDOMNode } from 'react-dom';
+import ExpensesChart from './ExpensesChart';
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState('2020');
+  const [filteredYear, setFilteredYear] = useState('2021');
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
@@ -24,6 +24,7 @@ const Expenses = (props) => {
           selectedYear={filteredYear}
           onFilterChange={filterChangeHandler}
         />
+        <ExpensesChart expenses={filteredExpenses} />
         <ExpensesList expenses={filteredExpenses} />
       </Card>
     </div>
